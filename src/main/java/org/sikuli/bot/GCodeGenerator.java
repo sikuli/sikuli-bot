04cd2d24ca313c7335d166471e9ad1b48878b43d
Ian <ianchar4@gmail.com>
2014-09-27 12:41:34 -0600

    Update GCodeGenerator.java
    Modified the code so that it remembers where the last click was. Also created a method to do swipe gestures and a method to have the maker bot finish and go back to home. Some values (particularly the offsets) may need to be adjusted for accuracy depending on when the stylus is attached.

--- src/main/java/org/sikuli/bot/GCodeGenerator.java
+++ src/main/java/org/sikuli/bot/GCodeGenerator.java
@@ -1,11 +1,13 @@
 package org.sikuli.bot;
 
+
 import java.awt.Point;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Vector;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GCodeGenerator {
 	
@@ -15,9 +17,10 @@ public class GCodeGenerator {
 	private int height;
 	private int width;
 	private String filePath;
-	private final int ZBOTTOM = 140;
-	private final int STYLUS_Y_OFFSET = -60;
-	private final int STYLUS_X_OFFSET = 0;
+	private Point lastClick;
+	private final int ZBOTTOM = 100;
+	private final int STYLUS_Y_OFFSET = 0;
+	private final int STYLUS_X_OFFSET = -55;
 	
 	/**
 	 * Constructor
@@ -29,11 +32,16 @@ public class GCodeGenerator {
 	 * @param filePath The file path where the generated gCode will be saved.
 	 */
 	public GCodeGenerator(Point origin, int length, int height, int width, String filePath){
-		this.origin = origin;
+		this.origin = new Point((int)(origin.getX() * -1), (int)(origin.getY() * -1));
 		this.length = length;
 		this.height = height;
 		this.width = width;
 		this.filePath = filePath;
+		this.lastClick = new Point(0,0);;
+	}
+	
+	private Point invertCoords(Point coord){
+		return new Point(1280-(int)coord.getX() ,800 - (int)coord.getY());
 	}
 	
 	/**
@@ -47,19 +55,38 @@ public class GCodeGenerator {
 		//Assume that screen is 600x800 pixels 
 		//double made to int (possible loss in precision)
 		//coordinates switched since screen is on its side
-		int x = (int)((coord.getX()/800)*length);
-		int y = (int)((coord.getY()/600)*height);
+		coord = invertCoords(coord);
+		int x = (int)((coord.getX()/1250)*length) * -1;
+		int y = (int)((coord.getY()/800)*height) * -1;
 		
-		if(x >= origin.getX() + 280)
-			x = (int)(280 - origin.getX());
-		if(x < 0)
+		//Account for origin offset (absolute position)
+		x = (int)(x + origin.getX() - STYLUS_X_OFFSET);
+		y = (int)(y + origin.getY() - STYLUS_Y_OFFSET);
+		
+		//Ensure X and Y are in the bound of the MakerBot
+		if(x <= origin.getX() - 275)
+			x = (int)(-280 - origin.getX())*-1;
+		if(x > 0)
 			x = 0;
-		if(y >= origin.getY() + 150)
-			y = (int)(150 - origin.getY());
-		if(y < 0)
+		if(y <= origin.getY() - 145)
+			y = (int)(-150 - origin.getY())*-1;
+		if(y > 0)
 			y = 0;
 		
-		return new Point(x, y);
+		Point tempClick = new Point(x,y);
+		
+		//Calculate relative to last click point (relative position)
+		x = (int)(x - lastClick.getX());
+		y = (int)(y - lastClick.getY());
+
+		lastClick = tempClick;
+		//For Debugging
+		System.out.println("X absolute: " + lastClick.getX());
+		System.out.println("Y absolute: " + lastClick.getY());
+		System.out.println("X movement: " + x);
+		System.out.println("Y movement: " + y);
+		return lastClick;
+		//return new Point(x, y);
 	}
 
 	/**
@@ -67,34 +94,86 @@ public class GCodeGenerator {
 	 * 
 	 * @param clickPt The point on the device where the maker bot should click. 
 	 */
-	public Vector<String> createClickVector(Point clickPt){
-		//Calculate the new coordinate
-		clickPt = findCoord(clickPt);
-		Vector<String> vect = new Vector<String>();
+	public List<String> createClickVector(Point clickPt){
+		List<String> vect = new ArrayList<String>();
 		//Write to vector
 			
 		//Set Up stuff
-		vect.add("M73 P2");
-        vect.add("M103");
-        vect.add("M73 P5");
-        vect.add("M73 P0");
-		vect.add("G21");
-        vect.add("G90");
-		vect.add("G162 X Y F2500");
-
-		vect.add("G92 X" + ((int)origin.getX() + STYLUS_X_OFFSET) + " Y" +
-                ((int)origin.getY() + STYLUS_Y_OFFSET) + " Z0");
+		vect.add("M73 P2 \n" + "M103 \n" + "M73 P5 \n" + "M73 P0 \n");
+		vect.add("G21 \n" + "G90 \n");
+		//vect.add("G162 X Y F2500 \n");
+		if(codeCount == 1)
+			vect.add("G92 X" + ((int)lastClick.getX()) + " Y" + ((int)lastClick.getY()) + " Z0 \n");
+		else
+			vect.add("G92 X" + ((int)lastClick.getX()) + " Y" + ((int)lastClick.getY()) + " Z-" + (ZBOTTOM - 30 -  width) + "\n");
+		//Calculate the new coordinate
+		clickPt = findCoord(clickPt);
 		
 		//Moving
-		vect.add("G1 Z-" + (ZBOTTOM - 30 - width) + " F2300"); //adjust to right height
-
-		vect.add("G1 X-" + ((int) clickPt.getX()) + " Y-" +
-                ((int) clickPt.getY()) + " F2300"); //Move to pt
-
-		vect.add("G1 Z-" + (ZBOTTOM - width - 1)); //Click
-		vect.add("G1 Z-" + (ZBOTTOM - 30 - width));
-		vect.add("G1 X0 Y0 Z0"); //Return home
-		// vect.add("M72 P1"); //Done music 
+		if(codeCount == 1)
+			vect.add("G1 Z-" + (ZBOTTOM - 30 - width) + " F2300 \n"); //adjust to right height
+		vect.add("G1 X" + ((int) clickPt.getX()) + " Y" + ((int) clickPt.getY()) + " F2300 \n"); //Move to pt
+		vect.add("G1 Z-" + (ZBOTTOM - width - 1) + " \n"); //Click
+		vect.add("G1 Z-" + (ZBOTTOM - 30 -  width) + " F2300 \n");
+		//vect.add("G1 X0 Y0 Z0 \n"); //Return home
+		//vect.add("M72 P1"); //Done music 
+		
+		return vect;
+	}
+	
+	/**
+	 * Given two coordinates on the screen where the device start and end swipe 
+	 * 
+	 * @param clickPt1 The point on the device where the maker bot should click.
+	 * @param clickPt2 The point the device should drag to
+	 */
+	public List<String> createSwipeVector(Point clickPt1, Point clickPt2){
+		List<String> vect = new ArrayList<String>();
+		//Write to vector
+			
+		//Set Up stuff
+		vect.add("M73 P2 \n" + "M103 \n" + "M73 P5 \n" + "M73 P0 \n");
+		vect.add("G21 \n" + "G90 \n");
+		//vect.add("G162 X Y F2500 \n");
+		if(codeCount == 1)
+			vect.add("G92 X" + ((int)lastClick.getX()) + " Y" + ((int)lastClick.getY()) + " Z0 \n");
+		else
+			vect.add("G92 X" + ((int)lastClick.getX()) + " Y" + ((int)lastClick.getY()) + " Z-" + (ZBOTTOM - 30 -  width) + "\n");
+		//Calculate the new coordinate
+		clickPt1 = findCoord(clickPt1);
+		clickPt2 = findCoord(clickPt2);
+		
+		//Moving
+		if(codeCount == 1)
+			vect.add("G1 Z-" + (ZBOTTOM - 30 - width) + " F2300 \n"); //adjust to right height
+		vect.add("G1 X" + ((int) clickPt1.getX()) + " Y" + ((int) clickPt1.getY()) + " F2300 \n"); //Move to pt
+		vect.add("G1 Z-" + (ZBOTTOM - width - 1) + " \n"); //Click
+		vect.add("G1 X" + ((int) clickPt2.getX()) + " Y" + ((int) clickPt2.getY()) + " F2300 \n"); //Drag
+		vect.add("G1 Z-" + (ZBOTTOM - 30 -  width) + " F2300 \n");
+		//vect.add("G1 X0 Y0 Z0 \n"); //Return home
+		//vect.add("M72 P1"); //Done music 
+		
+		return vect;
+	}
+	
+	/**
+	 * Creates code to send back to the origin
+	 * 
+	 * @param clickPt The point on the device where the maker bot should click. 
+	 */
+	public List<String> createFinishVector(){
+		List<String> vect = new ArrayList<String>();
+		//Write to vector
+			
+		//Set Up stuff
+		vect.add("M73 P2 \n" + "M103 \n" + "M73 P5 \n" + "M73 P0 \n");
+		vect.add("G21 \n" + "G90 \n");
+		vect.add("G162 X Y F2500 \n");
+		vect.add("G92 X" + 0 + " Y" + 0 + " Z0 \n");
+		
+		//Moving
+		vect.add("G1 X0 Y0 Z60 \n"); //Return home
+		vect.add("M72 P1"); //Done music 
 		
 		return vect;
 	}
@@ -106,7 +185,7 @@ public class GCodeGenerator {
 	 */
 	public void createClickCode(Point clickPt){
 		//Calculate the new coordinate
-		Vector<String> vect = createClickVector(clickPt);
+		List<String> vect = createClickVector(clickPt);
 		//Write to file
 		try{
 			File newCode = new File(filePath + "gCode" + codeCount +".gcode");
@@ -130,11 +209,76 @@ public class GCodeGenerator {
 		}
 	}
 	
+	/**
+	 * Given the coordinate on the screen where the device should click, gcode is generated 
+	 * 
+	 * @param clickPt The point on the device where the maker bot should click. 
+	 */
+	public void createSwipeCode(Point clickPt1, Point clickPt2){
+		//Calculate the new coordinate
+		List<String> vect = createSwipeVector(clickPt1, clickPt2);
+		//Write to file
+		try{
+			File newCode = new File(filePath + "gCode" + codeCount +".gcode");
+			newCode.createNewFile();
+			
+			FileWriter fw = new FileWriter(newCode.getAbsoluteFile());
+			BufferedWriter writer = new BufferedWriter(fw);
+			
+			for(String line: vect)
+			{
+				writer.write(line);
+			}
+			
+			writer.close();
+			
+			System.out.println("Done with instruction set" + codeCount);
+			codeCount++;
+			
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Given the coordinate on the screen where the device should click, gcode is generated 
+	 * 
+	 * @param clickPt The point on the device where the maker bot should click. 
+	 */
+	public void createFinishCode(){
+		//Calculate the new coordinate
+		List<String> vect = createFinishVector();
+		//Write to file
+		try{
+			File newCode = new File(filePath + "finish" + ".gcode");
+			newCode.createNewFile();
+			
+			FileWriter fw = new FileWriter(newCode.getAbsoluteFile());
+			BufferedWriter writer = new BufferedWriter(fw);
+			
+			for(String line: vect)
+			{
+				writer.write(line);
+			}
+			
+			writer.close();
+			
+			System.out.println("Done with instruction set" + codeCount);
+			codeCount++;
+			
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args){
 		//Piece of paper is 205x268 mm
-		GCodeGenerator test = new GCodeGenerator(new Point(60,60), 140, 80, 3, ".");
-		test.createClickCode(new Point (114,375));
-		test.createClickCode(new Point(0,0));
-		test.createClickCode(new Point(571, 150));
+		GCodeGenerator test = new GCodeGenerator(new Point(40,20), 218, 137, 3, "/Users/Ian Char/Documents/School/DLA/makerBotRobot/test/");
+		test.createClickCode(new Point (0,0));
+		test.createClickCode(new Point(1280,0));
+		test.createClickCode(new Point(1280, 800));
+		test.createClickCode(new Point(0, 800));
+		test.createSwipeCode(new Point(0,800), new Point(0,0));
+		test.createFinishCode();
 	}
 }
